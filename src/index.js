3e2c5b3be9fdebe1f8aa400f5495886f6218189f
Ramiz Ahmed <ramiz.ahmed.programmer@gmail.com>
2019-09-21 16:35:52 +0500

    UI

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
-import App from './App';
+import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
+import { configureStore } from './app/store/configureStore';
+import ScrollToTop from './app/common/util/ScrollToTop';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
+
+const rootEl = document.getElementById('root');
+
+let render = () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <ScrollToTop>
+          <App />
+        </ScrollToTop>
+      </BrowserRouter>
+    </Provider>,
+    rootEl
+  );
+};
+
+if (module.hot) {
+  module.hot.accept('./app/layout/App', () => {
+    setTimeout(render);
+  });
+}
+
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
